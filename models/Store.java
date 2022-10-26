c29909719b3449214ffedafafd0ff6306ffd2c63
Eduard <ghiuzaneduard@gmail.com>
2022-10-26 15:11:47 +0300

    added fields, constructor, getter and setter

--- models/Store.java
+++ models/Store.java
@@ -1,5 +1,21 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Store {
+    private ArrayList<Movie> movies;
+
+    public Store() {
+        movies = new ArrayList<Movie>();
+    }
+    
+    public Movie getMovie(int index) {
+        return new Movie(movies.get(index));
+    }
+
+    public void setMovie(Movie movie, int index) {
+        movies.set(index, new Movie(movie));
+    }
+
     
 }

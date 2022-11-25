675be089b80cd9b68fb7f11430456c8130069a53
Eduard <ghiuzaneduard@gmail.com>
2022-11-25 14:04:08 +0200

    added movieAdded method test

--- src/test/StoreTest.java
+++ src/test/StoreTest.java
@@ -1,5 +1,13 @@
 package src.test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.Before;
+import org.junit.Test;
+
+import src.main.models.Movie;
+import src.main.models.Store;
+
 public class StoreTest {
     Store store;
 
@@ -8,6 +16,11 @@ public class StoreTest {
         store = new Store();
         store.addMovie(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2));
         store.addMovie(new Movie("The Godfather", "Blue-Ray", 9.1));
+    }
+
+    @Test
+    public void movieAdded() {
+        assertTrue(store.contains(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2)));
     }
     
 }

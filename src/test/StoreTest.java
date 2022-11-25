0efee4eda188ab1916c7161975f6bbc04cfe373c
Eduard <ghiuzaneduard@gmail.com>
2022-11-25 14:03:22 +0200

    added setup method

--- src/test/StoreTest.java
+++ src/test/StoreTest.java
@@ -1,4 +1,13 @@
 package src.test;
+import org.junit.Before;
 public class StoreTest {
+    Store store;
+
+    @Before
+    public void setup() {
+        store = new Store();
+        store.addMovie(new Movie("The Shawshank Redemption", "Blue-Ray", 9.2));
+        store.addMovie(new Movie("The Godfather", "Blue-Ray", 9.1));
+    }
     
 }

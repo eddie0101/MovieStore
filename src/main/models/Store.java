b5f959469829639bdface1ef2f518f465da8dd91
Eduard <ghiuzaneduard@gmail.com>
2022-11-29 13:23:30 +0200

    refactored sellMovie, rentMovie and returnMovie that used action method

--- src/main/models/Store.java
+++ src/main/models/Store.java
@@ -1,6 +1,7 @@
 package src.main.models;
 
 import java.util.ArrayList;
+import java.util.stream.IntStream;
 
 public class Store {
     private ArrayList<Movie> movies;
@@ -40,42 +41,49 @@ public class Store {
                 .findFirst()
                 .orElse(-1);
     }
+
     public void sellMovie(String movieName) {
-        action(movieName, "sell");
+        //action(movieName, "sell");
+        if (!movies.get(getMovieIndex(movieName)).isAvailable()) {
+            throw new IllegalStateException("cannot sell a rented movie");
+        }
+        movies.removeIf((movie) -> movie.getName().equals(movieName));
     }
 
     public void rentMovie(String movieName) {
-        action(movieName, "rent");
+        //action(movieName, "rent");
+        movies.get(getMovieIndex(movieName)).setAvailable(false);
     }
 
     public void returnMovie(String movieName) {
-        action(movieName, "return");
+        //action(movieName, "return");
+        movies.get(getMovieIndex(movieName)).setAvailable(true);
     }
 
-    private void action(String movieName, String action) {
-        if (movies.isEmpty()) {
-            throw new IllegalStateException("Empty store");
-        }
-        if (movieName == null || movieName.isBlank()) {
-            throw new IllegalArgumentException("movie name cannot be null/blank");
-        }
-        for (int i = 0; i < movies.size(); i++) {
-            if (movies.get(i).getName().equals(movieName)) {
-                if (action.equalsIgnoreCase("sell")) {
-                    if (!movies.get(i).isAvailable()) {
-                        throw new IllegalStateException("cannot sell a rented movie");
-                    }
-                    movies.remove(i);
-                } else if (action.equalsIgnoreCase("rent")) {
-                    movies.get(i).setAvailable(false);
-                } else if (action.equalsIgnoreCase("return")) {
-                    movies.get(i).setAvailable(true);
-                } else {
-                    throw new IllegalArgumentException("action must be 'sell'/'rent'/'return'");
-                }
-            }
-        }
-    }
+    // private void action(String movieName, String action) {
+    //     if (movies.isEmpty()) {
+    //         throw new IllegalStateException("Empty store");
+    //     }
+    //     if (movieName == null || movieName.isBlank()) {
+    //         throw new IllegalArgumentException("movie name cannot be null/blank");
+    //     }
+    //     for (int i = 0; i < movies.size(); i++) {
+    //         if (movies.get(i).getName().equals(movieName)) {
+    //             if (action.equalsIgnoreCase("sell")) {
+    //                 if (!movies.get(i).isAvailable()) {
+    //                     throw new IllegalStateException("cannot sell a rented movie");
+    //                 }
+    //                 movies.remove(i);
+    //             } else if (action.equalsIgnoreCase("rent")) {
+    //                 movies.get(i).setAvailable(false);
+    //             } else if (action.equalsIgnoreCase("return")) {
+    //                 movies.get(i).setAvailable(true);
+    //             } else {
+    //                 throw new IllegalArgumentException("action must be 'sell'/'rent'/'return'");
+    //             }
+    //         }
+    //     }
+    // }
 
     public String toString() {
         String temp = "\n********** JAVA VIDEO STORE **********\n\n";
